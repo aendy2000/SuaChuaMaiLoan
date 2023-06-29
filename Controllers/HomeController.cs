c20c8870805ad73eab834f98ad6b33a657391f79
Dang Van Tuan <dangtuan3025@yahoo.com>
2023-06-29 17:54:33 +0700

    Add function view on customer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,17 +17,10 @@ namespace SuaChuaMaiLoan.Controllers
             string jsonTextProduct = System.IO.File.ReadAllText(filePathProduct);
             List<Products> resultProduct = JsonConvert.DeserializeObject<List<Products>>(jsonTextProduct);
 
-            string filePathFlavour = Server.MapPath("~/JsonModels/flavour.json");
-            string jsonTextFlavour = System.IO.File.ReadAllText(filePathFlavour);
-            List<Flavour> resultFlavour = JsonConvert.DeserializeObject<List<Flavour>>(jsonTextFlavour);
-            Session["lstFlavour"] = resultFlavour;
-
-            string filePathCategories = Server.MapPath("~/JsonModels/categories.json");
-            string jsonTextCategories = System.IO.File.ReadAllText(filePathCategories);
-            List<Categories> resultCategories = JsonConvert.DeserializeObject<List<Categories>>(jsonTextCategories);
-            Session["lstCategories"] = resultCategories;
-
-            return View("Index", resultProduct);
+            if(resultProduct.Count < 8 )
+                return View("Index", resultProduct.OrderByDescending(o => o.ID).Take(4));
+            else
+                return View("Index", resultProduct.OrderByDescending(o => o.ID).Take(8));
         }
 
         public ActionResult Product()
@@ -37,17 +30,7 @@ namespace SuaChuaMaiLoan.Controllers
             string jsonTextProduct = System.IO.File.ReadAllText(filePathProduct);
             List<Products> resultProduct = JsonConvert.DeserializeObject<List<Products>>(jsonTextProduct);
 
-            string filePathFlavour = Server.MapPath("~/JsonModels/flavour.json");
-            string jsonTextFlavour = System.IO.File.ReadAllText(filePathFlavour);
-            List<Flavour> resultFlavour = JsonConvert.DeserializeObject<List<Flavour>>(jsonTextFlavour);
-            Session["lstFlavour"] = resultFlavour;
-
-            string filePathCategories = Server.MapPath("~/JsonModels/categories.json");
-            string jsonTextCategories = System.IO.File.ReadAllText(filePathCategories);
-            List<Categories> resultCategories = JsonConvert.DeserializeObject<List<Categories>>(jsonTextCategories);
-            Session["lstCategories"] = resultCategories;
-
-            return View("Product", resultProduct);
+            return View("Product", resultProduct.OrderByDescending(o => o.ID).ToList());
         }
 
         [HttpPost]
---
+++ Models/SystemContact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuaChuaMaiLoan.Models
+{
+    public class SystemContact
+    {
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Facebook { get; set; }
+
+    }
+}
